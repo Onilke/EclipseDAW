06389b8345ad30a17dda856ef931ca6045503f6f
Onilke <tabi_pardo_negro@hotmail.com>
2024-10-14 13:46:33 +0200

    tercer commit

---
+++ PruebasBucles/src/traza2.java
@@ -0,0 +1,22 @@
+
+public class traza2 {
+
+	public static void main(String[] args) {
+		int m=30;
+		int i=5;
+		do {
+			i++;
+			m=m+(m%20);
+			m-=20;
+			System.out.println(m/i);
+		}while (m > -20);
+		System.out.println("m:"+m);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
